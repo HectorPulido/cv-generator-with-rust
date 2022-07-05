db853e0322370103464d9f48789cc1512db52517
Hector Pulido <hector.pulido@platzi.com>
2022-07-05 17:48:58 -0500

    updated cv generator

--- source/src/generator.rs
+++ source/src/generator.rs
@@ -1,31 +1,83 @@
 use comrak::{markdown_to_html, ComrakExtensionOptions, ComrakOptions};
 use serde_json::Value;
+use std::fs;
 use std::io::Write;
 use std::process::{Command, Stdio};
+use regex::Regex;
 
+#[derive(Debug)]
 pub struct Generator {
     title: String,
     theme: String,
+    extra_data: Option<serde_json::Value>,
 }
 
 impl Generator {
-    const CALLBACKS: [(&'static str, fn(&serde_json::Value) -> String); 7] = [
+    const CALLBACKS: [(&'static str, fn(&serde_json::Value) -> String); 9] = [
         ("intro", Generator::intro),
+        ("achievement", Generator::achievement),
+        ("title", Generator::title),
         ("description", Generator::description),
         ("rightImage", Generator::right_image),
-        ("techStack", Generator::tech_stack),
+        ("unorderedList", Generator::unordered_list),
         ("space", Generator::space),
         ("extra", Generator::extra),
         ("social", Generator::social),
     ];
 
+    fn format_value(input: &Value) -> String {
+        return input.to_string().replace("\"", "");
+    }
+
     fn config(&mut self, input: &serde_json::Value) {
-        self.theme = input["theme"].to_string();
-        self.title = input["title"].to_string();
+        let input = &input["data"];
+        self.theme = Generator::format_value(&input["theme"]);
+        self.title = Generator::format_value(&input["title"]);
+        self.extra_data = Some(input["extra_data"].clone())
+    }
+
+    pub fn new() -> Self {
+        return Self {
+            title: String::from("default"),
+            theme: String::from("default"),
+            extra_data: None
+        };
     }
 
     fn intro(input: &serde_json::Value) -> String {
         return format!("# {}\n", input["data"].as_str().unwrap());
+    }
+
+    fn title(input: &serde_json::Value) -> String {
+        return format!("## {}\n", input["data"].as_str().unwrap());
+    }
+
+    fn achievement(input: &serde_json::Value) -> String {
+        let title = input["data"]["title"].as_str().unwrap();
+
+        let mut achievement = if let Some(url) = input["data"].get("url") {
+            let url = url.as_str().unwrap();
+            format!("### <a href=\"{url}\">{title}</a>")
+        } else {
+            format!("### {title}")
+        };
+
+        let description: Vec<String> = input["data"]["list"]
+            .as_array()
+            .expect("Invalid json format")
+            .into_iter()
+            .map(|x| format!("- {}", x.as_str().unwrap()))
+            .collect();
+
+        let description = description.join("\n");
+        achievement = format!("{achievement}\n{description}");
+
+        if let Some(date_range) = input["data"].get("data_range") {
+            let date_range = date_range.as_str().unwrap();
+            achievement = format!("{achievement}</br><b>{date_range}</b>");
+        }
+
+        return achievement;
     }
 
     fn description(input: &serde_json::Value) -> String {
@@ -52,9 +104,9 @@ impl Generator {
         return format!("## {}", title);
     }
 
-    fn tech_stack(input: &serde_json::Value) -> String {
+    fn unordered_list(input: &serde_json::Value) -> String {
         let title = Self::format_title(input["data"]["title"].as_str().unwrap().to_string());
-        let tech: Vec<String> = input["data"]["tech"]
+        let tech: Vec<String> = input["data"]["list"]
             .as_array()
             .expect("Invalid json format")
             .into_iter()
@@ -65,7 +117,7 @@ impl Generator {
     }
 
     fn space(_: &serde_json::Value) -> String {
-        return String::from("<br>");
+        return String::from("</br>");
     }
 
     fn extra(input: &serde_json::Value) -> String {
@@ -97,22 +149,9 @@ impl Generator {
         return format!("{}\n<p align=\"center\">\n{}\n</p>\n", title, social_string);
     }
 
-    pub fn new() -> Self {
-        return Self {
-            title: String::from("default"),
-            theme: String::from("default"),
-        };
-    }
-
-    pub fn format(&mut self, json: Value) -> String {
-        let json_serde = json.as_array().expect("Invalid json format");
-        let mut markdown: String = String::from("");
-        for field in json_serde {
-            if field["type"] == "config" {
-                self.config(field);
-                continue;
-            }
-
+    fn format_section(&mut self, json: Value) -> String {
+        let mut markdown = String::from("");
+        for field in json.as_array().expect("Invalid json format") {
             for (name, callback) in &Self::CALLBACKS {
                 if *name == field["type"].as_str().expect("Invalid json format") {
                     markdown = format!("{}\n{}", markdown, callback(field));
@@ -120,11 +159,30 @@ impl Generator {
                 }
             }
         }
-
         return markdown;
     }
 
-    pub fn generate_html(markdown: String) -> String {
+    pub fn format(&mut self, json: Value) -> String {
+        let json_serde = json.as_array().expect("Invalid json format");
+        let mut html_data = String::from("");
+
+        for field in json_serde {
+            if field["type"] == "config" {
+                self.config(field);
+                continue;
+            }
+
+            if field["type"] == "section" {
+                let size = Generator::format_value(&field["size"]);
+                let formated_data = &self.format_section(field["data"].clone());
+                let generated_html = self.generate_html(formated_data.to_string());
+                html_data = format!("{html_data} <section class=\"col-md-{size}\">{generated_html}</section>");
+            }   
+        }
+        return html_data;
+    }
+
+    pub fn generate_html(&mut self, markdown: String) -> String {
         let mut opts = ComrakOptions {
             extension: ComrakExtensionOptions {
                 strikethrough: true,
@@ -144,58 +202,28 @@ impl Generator {
         return markdown_to_html(markdown.as_str(), &opts);
     }
 
-    pub fn apply_theme(html: String) -> String {
-        let template = r###"
-        <!doctype html>
-        <html lang="en">
-        <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">    
-        
-        <!-- Bootstrap core CSS -->
-        <link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-        
-        <style>
-            .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            user-select: none;
+    pub fn apply_theme(&mut self, html: String) -> String {
+        let mut template: String = fs::read_to_string(&self.theme).expect("Unable to read theme file");
+        template = template.replace("[[[html]]]", &html);
+        if let Some(extra_data) = &self.extra_data {
+            for (key, value) in extra_data.as_object().unwrap() {
+                let key = format!("[[[{}]]]", key);
+                let value = value.to_string().replace("\"", "");
+                template = template.replace(&key, &value);
             }
-        
-            @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-            }
-        </style>;
-        <!-- Custom styles for this template -->
-        <link href="https://getbootstrap.com/docs/5.0/examples/starter-template/starter-template.css" rel="stylesheet">
-        </head>
-        <body>
-            {{{}}}
-        </body>
-
-        <style>
-        img.emoji {
-        height: 1em;
-        width: 1em;
-        margin: 0 .05em 0 .1em;
-        vertical-align: -0.1em;
         }
-        </style>
-        <script src="https://twemoji.maxcdn.com/2/twemoji.min.js?11.2"></script>
-        <script>window.onload = function () { twemoji.parse(document.body);}</script>
 
-        </html>
-    "###;
+        let fa_re = Regex::new(r"\{i (.+)\}").unwrap();
+        template = fa_re.replace_all(&template, "<i class=\"$1\"></i>").to_string();
 
-        return template.replace("{{{}}}", &html);
+        println!("{template}");
+        return template;
     }
 
     pub fn generate_pdf(html: String, filename: &String) -> String {
         let mut child = Command::new("wkhtmltopdf")
+            .arg("--viewport-size")
+            .arg("1280x1024")
             .arg("-")
             .arg(filename)
             .stdin(Stdio::piped())
--- source/src/main.rs
+++ source/src/main.rs
@@ -1,24 +1,34 @@
 mod generator;
+
+use generator::Generator;
+
 use actix_web::dev::BodyEncoding;
 use actix_web::http::ContentEncoding;
 use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
-use generator::Generator;
 use rand::Rng;
 use std::env;
+use std::path::Path;
 use std::fs::{remove_file, File};
-use std::io::Read;
+use std::io::{Read, BufReader};
+
+fn create_pdf(json_data: serde_json::Value) -> String {
+    let mut rng = rand::thread_rng();
+    let random_name = format!("cv_{}.pdf", rng.gen_range(0..99999));
+    let mut generator = Generator::new();
+    let html = generator.format(json_data);
+    let html = generator.apply_theme(html);
+    Generator::generate_pdf(html, &random_name);
+
+    return random_name;
+}
+
 
 #[post("/generate_cv")]
 async fn generate_cv(info: web::Json<serde_json::Value>) -> impl Responder {
-    let mut rng = rand::thread_rng();
-    let random_name = format!("cv_{}.pdf", rng.gen_range(0..99999));
-
-    let mut generator = Generator::new();
-    let markdown = generator.format(info.into_inner());
-    let html = Generator::generate_html(markdown);
-    let final_html = Generator::apply_theme(html);
-    Generator::generate_pdf(final_html, &random_name);
+    let json_data = info.into_inner();
+    
+    let random_name = create_pdf(json_data);
 
     // File content may come from a database as a blob data
     let mut f = File::open(&random_name).expect("Failed to read file");
@@ -40,8 +50,23 @@ async fn generate_cv(info: web::Json<serde_json::Value>) -> impl Responder {
         .body(buffer)
 }
 
+fn read_json_from_file<P: AsRef<Path>>(path: P) -> serde_json::Value {
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    return serde_json::from_reader(reader).unwrap();
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if let Some(json_path) = args.get(1){
+        let json_data: serde_json::Value = read_json_from_file(json_path);
+        let random_name = create_pdf(json_data);
+        println!("Path of the generated cv: {random_name}");
+        return Ok(());
+    }
+
     dotenv().ok();
 
     let host = env::var("HOST").expect("HOST must be set");
